3c81972a21ca6c6039ac16d131233128fd4c5754
Om Prakash <omiiieee15@gmail.com>
2025-01-08 10:04:52 +0530

    updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,7 +5,7 @@ export const headerData = {
     name: 'Om Prakash',
     title: "Software Developer",
     // desciption:"There wasn't a breeze in the air, but that was not what caught his attention. It was the horizon. The eerie red that isn't the color of sunsets, but arrived with this one. He knew what was approaching and he hoped he was ready. ",
-    description : " Aspiring software developer with experience in web and application development using modern technologies like MERN Stack. Proven ability to enhance user experience and functionality through robust problem-solving and efficient coding practices ",
+    desciption : " Aspiring software developer with experience in web and application development using modern technologies like MERN Stack. Proven ability to enhance user experience and functionality through robust problem-solving and efficient coding practices ",
     // image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
     image: myProfile,
     resumePdf: resume
